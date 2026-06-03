08ed0122a9a09b7ae535ba2c03414f4ca722bf88
Zaqwan-Waseem <zaqwanwaseem@gmail.com>
2026-06-03 14:27:39 +0530

    Revert "Add Image"
    This reverts commit 47ab105759148c475f7db08eedf876b140dc0127.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -60,12 +60,6 @@ export default function Index() {
                       <Text className="h1-bold text-white leading-tight">
                         {item.title}
                       </Text>
-                      <Image
-                        source={images.arrowRight}
-                        className="size-10"
-                        resizeMode="contain"
-                        tintColor="#ffffff"
-                      />
                     </View>
                   </Fragment>
                 )}
