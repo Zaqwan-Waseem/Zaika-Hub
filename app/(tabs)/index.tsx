6fe7d732f0a11f31943a3399578c0554af3ee211
Zaqwan-Waseem <zaqwanwaseem@gmail.com>
2026-06-03 13:18:32 +0530

    Implement: Auth & Tabs functionalities

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import {
   Image,
   Pressable,
   Text,
-  TouchableOpacity,
+  TouchableOpacity, 
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
-import { SplashScreen, Stack } from "expo-router";
-import "./globals.css";
 import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
+import "./globals.css";
 
 export default function RootLayout() {
   const [fontsLoaded, error] = useFonts({
